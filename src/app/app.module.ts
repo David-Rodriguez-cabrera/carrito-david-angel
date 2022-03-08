713a1cfadf80213123ae1f682426e3182a046f32
David-Rodriguez-cabrera <david-rodriguez-cabrera@hotmail.com>
2022-03-08 13:47:44 +0100

    Video-4-Acabado

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module'
+import { BackendModule } from './backend/backend.module'
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(),
+    PagesModule,
+    BackendModule,
+    AppRoutingModule, 
+    ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
